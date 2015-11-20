e45e1b61058a1728b8d8719a84b7abb176bd683e
Krzysztof Trzepla <krzysztof.trzepla@gmail.com>
2015-11-20 16:17:23 +0100

    Initial commit.

---
+++ src/cellular_automaton_app.erl
@@ -0,0 +1,16 @@
+-module(cellular_automaton_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    cellular_automaton_sup:start_link().
+
+stop(_State) ->
+    ok.
